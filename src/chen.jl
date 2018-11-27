be21685935256bcdc39dee09c009b52e3604fabb
Mateusz Baran <mateuszbaran89@gmail.com>
2018-11-27 19:11:19 +0100

    API improvements, more tests
    * dims argument for all methods
    * DenseMatrix -> AbstractMatrix
    * rblw and oas are probably broken (lw looks OK though)

--- src/chen.jl
+++ src/chen.jl
@@ -4,49 +4,74 @@ using LinearAlgebra
 struct RBLWCovariance <: CovarianceEstimator
 end
 
-function chenshrinkagetarget(X::DenseMatrix{<:Real})
+function chenshrinkagetarget(X::AbstractMatrix{<:Real})
     p = size(X, 2)
     C = cov(X; dims=2)
     (tr(C)/p) * one(C)
 end
 
 """
-    cov(RBLWCovariance(), X, shrinkage)
+    cov(RBLWCovariance(), X::AbstractMatrix, shrinkage; dims::Int=1)
 
 Calculates shrunk covariance matrix for centered data `X` with shrinkage
 parameter `shrinkage` and Rao-Blackwell theorem Ledoit-Wolf shrinkage target.
 
+# Arguments
+- `dims::Int`: the dimension along which the variables are organized.
+When `dims = 1`, the variables are considered columns with observations
+in rows; when `dims = 2`, variables are in rows with observations in columns.
+
 Implements shrinkage target and optimal shrinkage according to
 Y. Chen, A. Wiesel, Y. C. Eldar, and A. O. Hero,
 “Shrinkage Algorithms for MMSE Covariance Estimation,”
 IEEE Transactions on Signal Processing, vol. 58, no. 10, pp. 5016–5029, Oct. 2010.
 """
-function cov(::RBLWCovariance, X::DenseMatrix{T}, shrinkage::Number) where T<:Real
+function cov(::RBLWCovariance, X::AbstractMatrix{T}, shrinkage::Number; dims::Int=1) where T<:Real
     (0 ≤ shrinkage ≤ 1) || throw(ArgumentError("Shinkage must be in [0,1] (given shrinkage: $shrinkage)"))
-    C = cov(X; dims=2)
-    F = chenshrinkagetarget(X)
+    if dims == 1
+        Xint = transpose(X)
+    elseif dims == 2
+        Xint = X
+    else
+        throw(ArgumentError("Argument dims can only be 1 or 2 (given: $dims)"))
+    end
+    C = cov(Xint; dims=2)
+    F = chenshrinkagetarget(Xint)
     (1-shrinkage)*C + shrinkage*F
 end
 
 """
-    cov(::RBLWCovariance, X::DenseMatrix)
+    cov(::RBLWCovariance, X::AbstractMatrix; dims::Int=1)
 
 Calculates shrunk covariance matrix for centered data `X` with optimal shrinkage
 for Rao-Blackwell theorem Ledoit-Wolf shrinkage target.
+
+# Arguments
+- `dims::Int`: the dimension along which the variables are organized.
+When `dims = 1`, the variables are considered columns with observations
+in rows; when `dims = 2`, variables are in rows with observations in columns.
 
 Implements shrinkage target and optimal shrinkage according to
 Y. Chen, A. Wiesel, Y. C. Eldar, and A. O. Hero,
 “Shrinkage Algorithms for MMSE Covariance Estimation,”
 IEEE Transactions on Signal Processing, vol. 58, no. 10, pp. 5016–5029, Oct. 2010.
 """
-function cov(::RBLWCovariance, X::DenseMatrix{T}) where T<:Real
-    C = cov(X; dims=2)
-    n, p = size(X)
+function cov(::RBLWCovariance, X::AbstractMatrix{T}; dims::Int=1) where T<:Real
+    if dims == 1
+        Xint = transpose(X)
+    elseif dims == 2
+        Xint = X
+    else
+        throw(ArgumentError("Argument dims can only be 1 or 2 (given: $dims)"))
+    end
+
+    C = cov(Xint; dims=2)
+    n, p = size(Xint)
     trS2 = dot(C, transpose(C)) # trace of C*C
     tr2S = tr(C)^2
     ρhat = ((n-2)/n * trS2 + tr2S)/((n+2) * (trS2 - tr2S/p))
     ρhat = min(ρhat, 1)
-    F = chenshrinkagetarget(X)
+    F = chenshrinkagetarget(Xint)
     (1-ρhat)*C + ρhat*F
 end
 
@@ -54,38 +79,56 @@ struct OASCovariance <: CovarianceEstimator
 end
 
 """
-    cov(OASCovariance(), X, shrinkage)
+    cov(OASCovariance(), X::AbstractMatrix, shrinkage; dims::Int=1)
 
 Calculates shrunk covariance matrix for centered data `X` with shrinkage
 parameter `shrinkage` and Rao-Blackwell theorem Ledoit-Wolf shrinkage target.
 
+# Arguments
+- `dims::Int`: the dimension along which the variables are organized.
+When `dims = 1`, the variables are considered columns with observations
+in rows; when `dims = 2`, variables are in rows with observations in columns.
+
 Implements shrinkage target and optimal shrinkage according to
 Y. Chen, A. Wiesel, Y. C. Eldar, and A. O. Hero,
 “Shrinkage Algorithms for MMSE Covariance Estimation,”
 IEEE Transactions on Signal Processing, vol. 58, no. 10, pp. 5016–5029, Oct. 2010.
 """
-function cov(::OASCovariance, X::DenseMatrix{T}, shrinkage::Number) where T<:Real
-    cov(RBLWCovariance(), X, shrinkage)
+function cov(::OASCovariance, X::AbstractMatrix{T}, shrinkage::Number; dims::Int=1) where T<:Real
+    cov(RBLWCovariance(), X, shrinkage; dims = dims)
 end
 
 """
-    cov(::OASCovariance, X::DenseMatrix)
+    cov(::OASCovariance, X::AbstractMatrix; dims::Int=1)
 
 Calculates shrunk covariance matrix for centered data `X` with optimal
 oracle approximating shrinkage estimator.
 
+# Arguments
+- `dims::Int`: the dimension along which the variables are organized.
+When `dims = 1`, the variables are considered columns with observations
+in rows; when `dims = 2`, variables are in rows with observations in columns.
+
 Implements shrinkage target and optimal shrinkage according to
 Y. Chen, A. Wiesel, Y. C. Eldar, and A. O. Hero,
 “Shrinkage Algorithms for MMSE Covariance Estimation,”
 IEEE Transactions on Signal Processing, vol. 58, no. 10, pp. 5016–5029, Oct. 2010.
 """
-function cov(::OASCovariance, X::DenseMatrix{T}) where T<:Real
-    C = cov(X; dims=2)
-    n, p = size(X)
+function cov(::OASCovariance, X::AbstractMatrix{T}; dims::Int=1) where T<:Real
+    if dims == 1
+        Xint = transpose(X)
+    elseif dims == 2
+        Xint = X
+    else
+        throw(ArgumentError("Argument dims can only be 1 or 2 (given: $dims)"))
+    end
+
+    C = cov(Xint; dims=2)
+    n, p = size(Xint)
     trS2 = dot(C, transpose(C)) # trace of C*C
     tr2S = tr(C)^2
     ρhat = (-1.0/p * trS2 + tr2S)/((n-1.0)/p * (trS2 - tr2S/p))
     ρhat = min(ρhat, 1)
-    F = chenshrinkagetarget(X)
+    F = chenshrinkagetarget(Xint)
     (1-ρhat)*C + ρhat*F
 end
--- src/ledoitwolf.jl
+++ src/ledoitwolf.jl
@@ -3,7 +3,7 @@ using LinearAlgebra
 struct LedoitWolfCovariance <: CovarianceEstimator
 end
 
-function ledoitwolfshrinkagetarget(C::DenseMatrix{<:Real})
+function ledoitwolfshrinkagetarget(C::AbstractMatrix{<:Real})
     N = size(C, 1)
     Cs = [sqrt(C[i,i]*C[j,j]) for i in 1:N, j in 1:N]
     r = C ./ Cs
@@ -14,41 +14,66 @@ function ledoitwolfshrinkagetarget(C::DenseMatrix{<:Real})
 end
 
 """
-    cov(LedoitWolfCovariance(), X, shrinkage)
+    cov(LedoitWolfCovariance(), X, shrinkage; dims::Int=1)
 
 Calculates shrunk covariance matrix for centered data `X` with shrinkage
 parameter `shrinkage` and Ledoit-Wolf shrinkage target.
+
+# Arguments
+- `dims::Int`: the dimension along which the variables are organized.
+When `dims = 1`, the variables are considered columns with observations
+in rows; when `dims = 2`, variables are in rows with observations in columns.
 
 Implements shrinkage target and optimal shrinkage according to
 O. Ledoit and M. Wolf, “Honey, I Shrunk the Sample Covariance Matrix,”
 The Journal of Portfolio Management, vol. 30, no. 4, pp. 110–119, Jul. 2004.
 """
-function cov(::LedoitWolfCovariance, X::DenseMatrix{T}, shrinkage::Number) where T<:Real
+function cov(::LedoitWolfCovariance, X::AbstractMatrix{T}, shrinkage::Number; dims::Int=1) where T<:Real
+    if dims == 1
+        Xint = transpose(X)
+    elseif dims == 2
+        Xint = X
+    else
+        throw(ArgumentError("Argument dims can only be 1 or 2 (given: $dims)"))
+    end
+
     (shrinkage ≥ 0 && shrinkage ≤ 1) || throw(ArgumentError("Shinkage must be in [0,1] (given shrinkage: $shrinkage)"))
-    C = cov(X; dims=2)
+    C = cov(Xint; dims=2)
     F, r̄ = ledoitwolfshrinkagetarget(C)
     (1-shrinkage)*C + shrinkage*F
 end
 
 """
-    cov(::LedoitWolfCovariance, X::DenseMatrix)
+    cov(::LedoitWolfCovariance, X::DenseMatrix; dims::Int=1)
 
 Calculates shrunk covariance matrix for centered data `X` with
 Ledoit-Wolf optimal shrinkage.
+
+# Arguments
+- `dims::Int`: the dimension along which the variables are organized.
+When `dims = 1`, the variables are considered columns with observations
+in rows; when `dims = 2`, variables are in rows with observations in columns.
 
 Implements shrinkage target and optimal shrinkage according to
 O. Ledoit and M. Wolf, “Honey, I Shrunk the Sample Covariance Matrix,”
 The Journal of Portfolio Management, vol. 30, no. 4, pp. 110–119, Jul. 2004.
 """
-function cov(::LedoitWolfCovariance, X::DenseMatrix{T}) where T<:Real
-    C = cov(X; dims=2)
-    Tnum = size(X, 2)
-    N = size(X, 1)
+function cov(::LedoitWolfCovariance, X::AbstractMatrix{T}; dims::Int=1) where T<:Real
+    if dims == 1
+        Xint = transpose(X)
+    elseif dims == 2
+        Xint = X
+    else
+        throw(ArgumentError("Argument dims can only be 1 or 2 (given: $dims)"))
+    end
+    C = cov(Xint; dims=2)
+    Tnum = size(Xint, 2)
+    N = size(Xint, 1)
     F, r̄ = ledoitwolfshrinkagetarget(C)
-    πmatrix = mean([(X[i,t]*X[j,t]-C[i,j])^2 for i in 1:N, j in 1:N] for t in 1:Tnum)
+    πmatrix = mean([(Xint[i,t]*Xint[j,t]-C[i,j])^2 for i in 1:N, j in 1:N] for t in 1:Tnum)
     πhat = sum(πmatrix)
-    ϑhatii = mean([(X[i,t]^2 - C[i,i])*(X[i,t]*X[j,t] - C[i,j]) for i in 1:N, j in 1:N] for t in 1:Tnum)
-    ϑhatjj = mean([(X[i,t]^2 - C[j,j])*(X[i,t]*X[j,t] - C[i,j]) for i in 1:N, j in 1:N] for t in 1:Tnum)
+    ϑhatii = mean([(Xint[i,t]^2 - C[i,i])*(Xint[i,t]*Xint[j,t] - C[i,j]) for i in 1:N, j in 1:N] for t in 1:Tnum)
+    ϑhatjj = mean([(Xint[i,t]^2 - C[j,j])*(Xint[i,t]*Xint[j,t] - C[i,j]) for i in 1:N, j in 1:N] for t in 1:Tnum)
     ρhatpart2 = zero(T)
     #TODO: inbounds/simd?
     for i in 1:N
@@ -59,6 +84,11 @@ function cov(::LedoitWolfCovariance, X::DenseMatrix{T}) where T<:Real
     ρhat = sum(diag(πmatrix)) + (r̄/2)*ρhatpart2
     γhat = sum((F - C).^2)
     κhat = (πhat - ρhat)/γhat
-    δstar = clamp(κhat/Tnum, 0.0, 1.0)
+    if abs(γhat) < 1e-16
+        #division by almost zero, co shrinkage doesn't really matter
+        δstar = 0.0
+    else
+        δstar = clamp(κhat/Tnum, 0.0, 1.0)
+    end
     (1-δstar)*C + δstar*F
 end
