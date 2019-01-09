14de2954c252d06720afc1e6cc5f93fe71916f93
Thibaut Lienart <tlienart@me.com>
2019-01-09 20:25:29 +1100

    centercols is now useless -> removing

--- src/utils.jl
+++ src/utils.jl
@@ -1,26 +1,3 @@
-"""
-    centercols(X, μ; dims)
-
-Internal function to center the columns of `X` given `μ` which can either be
-    * `nothing` (in which case the mean is estimated and subtracted from `X`)
-    * `0` (in which case nothing is done, the matrix is assumed to be centred)
-    * a given vector (in which case that vector is subtracted from `X`)
-"""
-function centercols(X::AbstractMatrix, μ=nothing; dims=1)
-    Xc = (dims == 1) ? copy(X) : transpose(X)
-    if μ === nothing
-        Xc .-= mean(X, dims=1)
-    elseif iszero(μ)
-        # assume X is centered already
-    elseif μ isa AbstractVector
-        @assert length(μ) == size(Xc, 2) "Dimension mismatch, given mean has length $(length(μ)) while p=$(size(Xc, 2))"
-        Xc .-= μ
-    else
-        throw(ArgumentError("Incorrect assignment for `mean`, expected either 0, nothing or a vector, got $μ."))
-    end
-    Xc
-end
-
 function linshrink(F::Union{UniformScaling, AbstractMatrix},
                    S::AbstractMatrix, λ::Real)
     return (1.0 - λ)*S + λ*F
--- test/legacy.jl
+++ test/legacy.jl
@@ -1,6 +1,6 @@
-# NOTE: these are older implementations which were used in the past but
-# have now been optimised heavily and ultimately replaced by other functions.
-# they're only kept here for the tests.
+# NOTE: these are older implementations/functions which were used in the past
+# but have now been optimised heavily and ultimately replaced by other
+# functions they're only kept here for the tests.
 
 function sum_var_sij(Z::AbstractMatrix, CovZ::AbstractMatrix, n::Int,
                      corrected=false; with_diag=true)
@@ -9,4 +9,19 @@ function sum_var_sij(Z::AbstractMatrix, CovZ::AbstractMatrix, n::Int,
     π̂mat  = ((Z²'*Z²)/n - CovZ.^2) / scale
     with_diag && return sum(π̂mat)
     return sum(π̂mat) - sum(diag(π̂mat))
+end
+
+function centercols(X::AbstractMatrix, μ=nothing; dims=1)
+    Xc = (dims == 1) ? copy(X) : transpose(X)
+    if μ === nothing
+        Xc .-= mean(X, dims=1)
+    elseif iszero(μ)
+        # assume X is centered already
+    elseif μ isa AbstractVector
+        @assert length(μ) == size(Xc, 2) "Dimension mismatch, given mean has length $(length(μ)) while p=$(size(Xc, 2))"
+        Xc .-= μ
+    else
+        throw(ArgumentError("Incorrect assignment for `mean`, expected either 0, nothing or a vector, got $μ."))
+    end
+    Xc
 end
