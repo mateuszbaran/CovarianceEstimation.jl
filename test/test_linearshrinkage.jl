b9da5f1660ee0326d05c658024e7bcc902d36085
Mateusz Baran <mateuszbaran89@gmail.com>
2019-03-06 09:40:33 +0100

    Enabled an additional ConstantCorrelation test

--- test/test_linearshrinkage.jl
+++ test/test_linearshrinkage.jl
@@ -160,7 +160,7 @@ end
             DiagonalUnequalVariance(),
             CommonCovariance(),
             PerfectPositiveCorrelation(),
-            # ConstantCorrelation()
+            ConstantCorrelation()
             ]
         #=
         :ss assumes that the shrinkage should be the same if the data
