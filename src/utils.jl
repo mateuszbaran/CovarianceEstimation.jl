f94fef9998e66f0f962c767ce1ea42ff5745bcd6
Lyndon White <oxinabox@ucc.asn.au>
2020-07-24 19:08:33 +0100

    use fused broadcasting in core linshrink operation

--- src/utils.jl
+++ src/utils.jl
@@ -1,11 +1,10 @@
 function linshrink(F::Union{UniformScaling, AbstractMatrix},
                    S::AbstractMatrix, λ::Real)
-    return Symmetric((1.0 - λ)*S + λ*F)
+    return Symmetric((1.0 .- λ).*S .+ λ.*F)
 end
 
 
 function linshrink!(F::AbstractMatrix, S::AbstractMatrix, λ::Real)
-    F .*= λ
-    F .+= (1.0 - λ)*S
+    F .= (1.0 .- λ).*S .+ λ.*F)
     return Symmetric(F)
 end
