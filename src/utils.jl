a430bcc8c2ac4e69c17a8589d42dbb62a8debee5
Lyndon White <oxinabox@ucc.asn.au>
2020-07-24 22:49:35 +0100

    seperate for UniformScaling

--- src/utils.jl
+++ src/utils.jl
@@ -1,8 +1,10 @@
-function linshrink(F::Union{UniformScaling, AbstractMatrix},
-                   S::AbstractMatrix, λ::Real)
+function linshrink(F::AbstractMatrix, S::AbstractMatrix, λ::Real)
     return Symmetric((1.0 .- λ).*S .+ λ.*F)
 end
 
+function linshrink(F::UniformScaling, S::AbstractMatrix, λ::Real)
+    return Symmetric((1.0 .- λ).*S + λ.*F)
+end
 
 function linshrink!(F::AbstractMatrix, S::AbstractMatrix, λ::Real)
     F .= (1.0 .- λ).*S .+ λ.*F
