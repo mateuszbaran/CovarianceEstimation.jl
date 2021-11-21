eadfb1c7643f6fb0db240c3f7452950b62050184
Tom Gillam <tpgillam@googlemail.com>
2021-11-21 23:53:55 +0000

    Test that other dispatches are consistent

--- test/test_biweight.jl
+++ test/test_biweight.jl
@@ -1,3 +1,20 @@
+function _test_cov_vec(ce::CovarianceEstimator, X::AbstractMatrix{<:Real})
+    c = cov(ce, X)
+    @inbounds for i in 1:size(X, 2)
+        @test cov(ce, @view(X[:, i])) ≈ c[i, i] atol = 1e-12 rtol = 1e-16
+    end
+end
+
+function _test_cov_vecvec(ce::CovarianceEstimator, X::AbstractMatrix{<:Real})
+    c = cov(ce, X)
+    @inbounds for i in 1:size(X, 2)
+        for j in 1:size(X, 2)
+            cij = cov(ce, @view(X[:, i]), @view(X[:, j]))
+            @test cij ≈ c[i, j] atol = 1e-12 rtol = 1e-16
+        end
+    end
+end
+
 @testset "BiweightMidcovariance: c=9.0, modify_sample_size=false" begin
     ce = BiweightMidcovariance()
 
@@ -8,6 +25,10 @@
     testTransposition(ce, test_mat1)
     testUncorrelated(ce)
     testTranslation(ce, test_mat1)
+
+    # Test that other dispatches are consistent with the main one we are testing.
+    _test_cov_vec(ce, test_mat1)
+    _test_cov_vecvec(ce, test_mat1)
 
     c1 = cov(ce, test_mat1)
     # TODO compare against references
