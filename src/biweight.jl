f7b59240ea40f983b89e8de64db0530250ccf7d0
Tom Gillam <tpgillam@googlemail.com>
2021-11-22 00:28:46 +0000

    More docs

--- src/biweight.jl
+++ src/biweight.jl
@@ -1,20 +1,23 @@
 """
-    BiweightMidcovariance(c=9.0)
+    BiweightMidcovariance(; c=9.0, modify_sample_size=false)
 
 The biweight midcovariance is a covariance estimator that is resilient to outliers.
+
+The technique derives originally from astrophysics [1], and is implemented in the Python
+module [Astropy](https://www.astropy.org/) [2], as well as in NIST's Dataplot [3].
 
 Consider two random variables ``x`` and ``y``, for which we have ``n`` observations
 ``\\{(x_i, y_i)\\}``. The biweight midcovariance is then defined to be:
 ```math
-n\\cdot\\frac{
+n_s\\cdot\\frac{
     \\sum_{|u_i|<1,|v_i|<1}(x_i - M_x)(1 - u_i^2)^2(y_i - M_y)(1 - v_i^2)^2
 }{
     \\left(\\sum_{|u_i|<1}(1 - u_i^2)(1-5u_i^2)\\right)
     \\left(\\sum_{|v_i|<1}(1 - v_i^2)(1-5v_i^2)\\right)
 }
 ```
-where ``M_x`` and ``M_y`` are the medians of ``\\{x_i\\}`` and ``\\{y_i\\}`` respectively,
-and
+where ``n_s`` is the sample size, ``M_x`` and ``M_y`` are the medians of ``\\{x_i\\}`` and
+``\\{y_i\\}`` respectively, and
 ```math
 \\begin{aligned}
 u_i &= \\frac{x_i - M_x}{c \\cdot \\mathrm{MAD}_x} \\\\
@@ -32,6 +35,27 @@ where ``\\mathrm{MAD}`` represents the median absolute deviation,
 The parameter ``c`` is a tuning constant, for which the default is ``9.0``.
 Larger values will reduce the number of outliers that are removed — i.e. reducing
 robustness, but increasing sample efficiency.
+
+# Fields
+- `c::Float64`: The tuning constant corresponding to ``c`` above.
+- `modify_sample_size::Bool`: If `false`, then we use a sample size ``n_s`` equal to the total
+    number of observations ``n``. This is consistent with the standard definition of
+    biweight midcovariance in the literature. Otherwise, we count only those elements which
+    are not rejected as outliers in the numerator, i.e. those for which ``|u_i|<1``
+    and ``|v_i|<1``.
+    This follows the implementation in astropy [2].
+
+# Complexity
+- Space: ``O(p^2)``
+- Time: ``O(np^2)``
+
+# References
+[1] Beers, Flynn, and Gebhardt (1990; AJ 100, 32)
+"Measures of Location and Scale for Velocities in Clusters of Galaxies -- A Robust Approach"
+
+[2] [Astropy biweight_midcovariance](https://docs.astropy.org/en/stable/api/astropy.stats.biweight_midcovariance.html)
+
+[3] [NIST Dataplot biweight midcovariance](https://www.itl.nist.gov/div898/software/dataplot/refman2/auxillar/biwmidc.htm)
 """
 struct BiweightMidcovariance <: CovarianceEstimator
     c::Float64
@@ -113,18 +137,16 @@ function cov(
 )
     dims ∈ (1, 2) || throw(ArgumentError("Argument dims can only be 1 or 2 (given: $dims)"))
 
-    # Deal with the `mean` argument -- if it isn't provided, then we centralise with the
-    # median.
-    MX = isnothing(mean) ? median(X; dims=dims) : mean
-    X = X .- MX
+    # If the `mean` argument isn't provided, then we centralise with the median.
+    Xc = isnothing(mean) ? X .- median(X; dims=dims) : X .- mean
 
-    # Standardise the orientation of X, and ascertain the number of variables.
-    X = dims == 2 ? transpose(X) : X
-    p = size(X, 2)
+    # Standardise the orientation of Xc, and ascertain the number of variables.
+    Xc = dims == 2 ? transpose(Xc) : Xc
+    p = size(Xc, 2)
 
     # Infer the output element type. This follows the approach used in the stdlib, in
     # Statistics.covzm.
-    T = promote_type(typeof(one(eltype(X)) / 1), eltype(X))
+    T = promote_type(typeof(one(eltype(Xc)) / 1), eltype(Xc))
 
     # Allocate a concrete output buffer, and populate it by iterating over all pairs.
     # Since different outliers may be removed
@@ -132,9 +154,9 @@ function cov(
     @inbounds for i in 1:p
         for j in i:p
             if i == j
-                result[i, j] = covzm(ce, @view(X[:, i]))
+                result[i, j] = covzm(ce, @view(Xc[:, i]))
             else
-                cov_ij = covzm(ce, @view(X[:, i]), @view(X[:, j]))
+                cov_ij = covzm(ce, @view(Xc[:, i]), @view(Xc[:, j]))
                 result[i, j] = cov_ij
                 result[j, i] = cov_ij
             end
